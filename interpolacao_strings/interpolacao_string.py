bf3ad5b419fd0d2d5ab63f0fea14d2f9d2796ea8
Nathan Carvalho <nthancdc320@gmail.com>
2023-08-07 11:43:45 -0300

    Interpolação de string e operadores aritiméticos

---
+++ interpolacao_strings/interpolacao_string.py
@@ -0,0 +1,12 @@
+nome = input("Digite o seu primeiro nome:")
+idade = int(input("Digite sua idade:"))
+
+'''
+print("Olá,",nome,"!")
+print("Tudo bem com você?")
+'''
+print("Olá, {}!".format(nome))
+print("Tudo bem com você?")
+#print("Caramba",nome,"! Você tem",idade,"anos? Nem parece.")
+#print("Caramba {}! Você tem {} anos? Nem parece, {}.".format(nome,idade,nome))
+print(f"Caramba {nome}! Você tem {idade} anos? Nem parece, {nome}.")
